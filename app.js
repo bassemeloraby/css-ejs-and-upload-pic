871d02c92c409d2e4a2a48305e3972cc3c09de33
bassem eloraby <BASEM12683@GMAIL.COM>
2022-05-03 11:39:21 +0300

    add files together

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 //add express
 const express = require('express')
 const app = express()
-const port =3000
+
+
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+  
+const fs = require('fs');
+const path = require('path');
+require('dotenv/config');
+
+// Step 4 - set up EJS
+  
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 //static file
 app.use(express.static('public'))
 app.use('/css',express.static(__dirname + 'public/css'))
@@ -12,11 +26,82 @@ app.use('/img',express.static(__dirname + 'public/img'))
 app.set('views','./views')
 app.set('view engine', 'ejs')
 
+// Step 2 - connect to the database
+
+mongoose.connect(process.env.MONGO_URL,
+	{ useNewUrlParser: true, useUnifiedTopology: true }, err => {
+		console.log('connected to Mongo Dp...')
+	});
+
+// Step 5 - set up multer for storing uploaded files
+
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'uploads')
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.fieldname + '-' + Date.now())
+	}
+});
+
+var upload = multer({ storage: storage });
+
+// Step 6 - load the mongoose model for Image
+
+var imgModel = require('./model');
+
+// Step 7 - the GET request handler that provides the HTML UI
+
+app.get('/', (req, res) => {
+	imgModel.find({}, (err, items) => {
+		if (err) {
+			console.log(err);
+			res.status(500).send('An error occurred', err);
+		}
+		else {
+			res.render('imagesPage', { items: items });
+		}
+	});
+});
+// Step 8 - the POST handler for processing the uploaded file
+
+app.post('/', upload.single('image'), (req, res, next) => {
+
+	var obj = {
+		name: req.body.name,
+		desc: req.body.desc,
+		img: {
+			data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+			contentType: 'image/png'
+		}
+	}
+	imgModel.create(obj, (err, item) => {
+		if (err) {
+			console.log(err);
+		}
+		else {
+			// item.save();
+			res.redirect('/');
+		}
+	});
+});
 
 
-app.get('',(req,res)=>{
+app.get('/bassem',(req,res)=>{
     res.render('index',{text:'this an EJS file'});
 })
 
 //create server at port
-app.listen(port,console.log(`express server work now at:${port}`))
+// const port =3000
+// app.listen(port,console.log(`express server work now at:${port}`))
+
+// Step 9 - configure the server's port
+
+var port = process.env.PORT || '3000'
+app.listen(port, err => {
+	if (err)
+		throw err
+	console.log('Server listening on port', port)
+})
